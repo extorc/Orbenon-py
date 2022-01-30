a207538fc922f1384e13fd5ae99cc5e5756d6207
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-01-30 19:55:38 +0530

    implementation for gravity : broken

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import pygame
 from pygame.locals import *
 from window import Window
 from object import Object
+import math
 
 WIN = Window(800, 600, "Orbenon")
-object = Object(10,10,0,300, 10000000)
-object2 = Object(10,10,100,200, 10000000)
+object = Object(10,10,0,300, 1000000000000000)
+object2 = Object(10,10,100,200, 1)
 
 G = 6.6e-11
 while WIN.running:
@@ -13,6 +14,11 @@ while WIN.running:
     if event.type == pygame.QUIT:
       WIN.running = False
 
+  fdir = (object.posX-object2.posX,object.posY-object2.posY)
+  f = G * (object.mass * object2.mass)/Object.get_distance(object, object2)**2
+  object2.acceleration_x = f * math.cos(math.atan((object.posY-object2.posY)/(object.posX-object2.posX)))/object2.mass
+  object2.acceleration_y = f * math.cos(math.atan((object.posY-object2.posY)/(object.posX-object2.posX)))/object2.mass
+  
   object.update()
   object2.update()
 
@@ -22,7 +28,4 @@ while WIN.running:
   
   pygame.display.update()
   dt = WIN.clock.tick(30)
-  object.set_acceleration_y(-9.8 * (dt/1000))
-  object2.set_acceleration_y(-9.8 * (dt/1000))
-
-  print((G * object.mass * object2.mass)/Object.get_distance(object, object2)**2)
+  print(object2.velocity_x)
