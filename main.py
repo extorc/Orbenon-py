a762e22841cab80e9ce20b98e603ddb7209e1f42
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-01-30 15:35:02 +0530

    Velocity

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from object import Object
 
 WIN = Window(800, 600, "Orbenon")
 object = Object(10,10,0,0)
+object.set_velocity_x(10)
+object.set_velocity_y(10)
 
 while WIN.running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       WIN.running = False
 
+  object.posY+=object.velocity_y
+  object.posX+=object.velocity_x
+
   WIN.screen.fill((200,200,200))
   WIN.drawAt((0,0,0),object)
+  
   pygame.display.update()
   dt = WIN.clock.tick(30)
   print(dt)
--- object.py
+++ object.py
@@ -6,3 +6,13 @@ class Object:
     self.sizeY = sizeY
     self.posX = posX
     self.posY = posY
+    self.velocity_x = 0
+    self.velocity_y = 0
+    
+  def set_velocity_y(self, y):
+    self.velocity_y = y
+  def set_velocity_x(self, x):
+    self.velocity_x = x
+  def set_velocity(self, x, y):
+    self.velocity_y = y
+    self.velocity_x = x
