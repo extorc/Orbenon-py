b7f968d7891763193301766aab8148e7bbe3e2de
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-01-31 14:20:06 +0530

    Basic Start and Stop UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_gui
 from pygame.locals import *
 from window import Window
 from object import Object
@@ -7,28 +8,49 @@ from physics_engine import PhysicsEngine
 import math
 
 WIN = Window(800, 600, "Orbenon")
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('#000000'))
+
 object = Object(10,10,400,200, 10000000000000)
 object2 = Object(10,10,500,100, 1)
 
 object2.set_velocity_y(0.2)
 object2.set_velocity_x(0.2)
-pointList = [(500,600-100-10)]
+pointList = [(500,600-100-10),(500,600-100-10)]
+
+manager = pygame_gui.UIManager((800, 600))
+start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((5,5), (70, 45)),text='Start',manager=manager)
+pause_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((80,5), (80, 45)),text='Pause',manager=manager)
+
+physics_running = False
 
 while WIN.running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       WIN.running = False
+    if event.type == pygame_gui.UI_BUTTON_PRESSED:
+      if event.ui_element == start_button:
+        print('Started Physics Simulation')
+        physics_running = True
+      elif event.ui_element == pause_button:
+        print('Paused Physics Simulation')
+        physics_running = False
+    manager.process_events(event)
 
   dt_factor = WIN.clock.tick(30)
   dt = dt_factor/1000
-  f = PhysicsEngine.get_newtonian_force(object, object2)
-  dir = Vec2(object.pos.x-object2.pos.x,object.pos.y-object2.pos.y)
-  dir_angle = math.atan2(dir.x,dir.y)
-  object2.set_acceleration_x(dt * f * math.sin(dir_angle)/object2.mass)
-  object2.set_acceleration_y(dt * f * math.cos(dir_angle)/object2.mass)
+  manager.update(dt)
 
-  pointList.append((object2.pos.x,600 - object2.pos.y-10))
+  if physics_running:
+    f = PhysicsEngine.get_newtonian_force(object, object2)
+    dir = Vec2(object.pos.x-object2.pos.x,object.pos.y-object2.pos.y)
+    dir_angle = math.atan2(dir.x,dir.y)
+    object2.set_acceleration_x(dt * f * math.sin(dir_angle)/object2.mass)
+    object2.set_acceleration_y(dt * f * math.cos(dir_angle)/object2.mass)
 
-  object.update()
-  object2.update()
-  WIN.update(pointList, object, object2)
+    pointList.append((object2.pos.x,600 - object2.pos.y-10))
+
+    object.update()
+    object2.update()
+
+  WIN.update(background, manager, pointList, object, object2)
--- window.py
+++ window.py
@@ -17,9 +17,12 @@ class Window:
 
   def drawAt(self, color, object):
     pygame.draw.rect(self.screen,color,(object.pos.x,self.winY-object.pos.y-object.sizeY, object.sizeX, object.sizeY))
-  def update(self, pointList, object, object2):
+    
+  def update(self, background, manager, pointList, object, object2):
+    self.screen.blit(background,(0,0))
     self.screen.fill((200,200,200))
     pygame.draw.lines(self.screen, (255,0,0), False, pointList)
     self.drawAt((0,0,0),object)
     self.drawAt((0,0,0),object2)
+    manager.draw_ui(self.screen)
     pygame.display.update()
